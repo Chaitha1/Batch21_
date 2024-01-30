281320e78a1210326d9461a50a40aae383513d19
Chaithra1 <R20200581.CHAITHRA@cse.reva.edu.in.com>
2024-01-30 23:57:11 +0530

    Second commit

---
+++ src/com/package1/rama/PersonOut.java
@@ -0,0 +1,19 @@
+package com.package1.rama;
+
+public class PersonOut {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Person obj = new Person();
+      
+      String address;
+	obj.setAddress("Nelamangala");
+	obj.setAge(22);
+	obj.setName("Chaithra");
+	
+	 System.out.println(obj);
+	
+      
+	}
+
+}
---
+++ src/com/package2/rama/Personin.java
@@ -0,0 +1,17 @@
+package com.package2.rama;
+
+// import the package
+import com.package1.rama.*;
+public class Personin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person obj=new Person();
+		
+		obj.setAddress("Bangalore");
+		obj.setAge(29);
+		obj.setName("Bubba");
+      System.out.println(obj);
+	}
+
+}
